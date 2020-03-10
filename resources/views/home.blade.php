2264484f3bd848a627a97336ab40054d5c7bff68
yonyen99 <yonyen99@gmial.com>
2020-03-10 20:06:59 +0700

    form login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,11 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+           
+            {{-- add tutor --}}
+          
+        <a class="nav-link" href="{{ route('register') }}">{{ __('Add Tutor') }}</a>
+                             
         </div>
     </div>
 </div>
